c5bb52b7451964062baef849e17a9264207ef2f0
kondo-riyo <r1y0.tenten.1126@gmail.com>
2023-07-17 18:23:52 +0900

    バリデーションの追加

--- resources/js/Pages/Inertia/Create.vue
+++ resources/js/Pages/Inertia/Create.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia' 
+
+defineProps({
+    errors: Object
+})
+
 const form = reactive({
     title: null,
     content: null 
@@ -12,6 +17,9 @@ const submitFunction = () => {
 </script> 
 
 <template>
+<div v-if="errors.title" class="text-red-500">{{ errors.title }}</div>
+<div v-if="errors.content" class="text-red-500">{{ errors.content }}</div>
+
 <form @submit.prevent="submitFunction">
     <input type="text" name="title" v-model="form.title">
     <input type="text" name="content" v-model="form.content"> 
