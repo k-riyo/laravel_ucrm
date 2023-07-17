af8d84d75f3614913527ec9b586368f4ecc1aa83
kondo-riyo <r1y0.tenten.1126@gmail.com>
2023-07-17 21:26:18 +0900

    defineEmitsの学習、適用

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,15 @@
 import Label from '@/Components/InputLabel.vue' 
 import Input from '@/Components/TextInput.vue' 
 import GuestLayout from '@/Layouts/GuestLayout.vue'
+
+const emitTest = e => console.log(e)
+
 </script>
 
 <template>
 <GuestLayout>
     <Label>タイトル</Label>
     <Input modelValue="初期値です"/>
+    <Input @update:modelValue="emitTest"></Input>
 </GuestLayout> 
 </template>
